7871a470444e7e31c67edf9bd00d69dd26702903
Izanami49 <elihu.montoya.estrada@gmail.com>
2026-06-14 15:54:46 -0600

    Creando las Primeras 3 entidades

---
+++ src/main/java/com/eime/storeshope/StoreShopeApplication.java
@@ -0,0 +1,13 @@
+package com.eime.storeshope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreShopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreShopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eime/storeshope/StoreShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eime.storeshope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreShopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
