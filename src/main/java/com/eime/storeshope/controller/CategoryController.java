2994d4aec33d7d6903b2278a96875b0c2315be1c
Izanami49 <elihu.montoya.estrada@gmail.com>
2026-06-18 19:13:12 -0600

    Creacion del Controllador ImageController

---
+++ src/main/java/com/eime/storeshope/controller/CategoryController.java
@@ -0,0 +1,4 @@
+package com.eime.storeshope.controller;
+
+public class CategoryController {
+}
---
+++ src/main/java/com/eime/storeshope/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.eime.storeshope.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+
+    private String message;
+    private Object data;
+
+
+}
